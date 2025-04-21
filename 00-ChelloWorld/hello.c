2f995f37cc2b22f435e135af4832a4ae24e3bd59
Guido <gbassi@frba.utn.edu.ar>
2025-04-21 16:09:54 -0300

    cambios stdout

--- 00-ChelloWorld/hello.c
+++ 00-ChelloWorld/hello.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    FILE *file = fopen("output.txt", "w");
-    if (file == NULL) {
-        perror("Error al abrir el archivo");
-        return 1;
-    }
-
-    fprintf(file, "Hola Guido Bassi!");
-    fclose(file);
+    printf("Hola Guido Bassi!\n");
     return 0;
 }
